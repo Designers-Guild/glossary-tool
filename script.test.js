ab3cb1847c33c4d07eef8d2c6c1c6ea607e078ca
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-04-24 12:32:18 +0200

    test

--- script.test.js
+++ script.test.js
@@ -4,7 +4,6 @@ describe("requestSynonym function", () => {
   test("should return a synonym for the provided word", async () => {
     const word = "happy";
     const synonym = await requestSynonym(word);
-    expect(synonym).toBeDefined();
     expect(synonym).not.toBe("");
     expect(typeof synonym).toBe("string");
   });
