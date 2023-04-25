47737c4bc49aee67772faa263b836ef5859a9a81
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-04-25 09:22:49 +0200

    test

--- script.test.js
+++ script.test.js
@@ -1,9 +1,32 @@
 const { requestSynonym } = require("./src/script");
-jest.mock('webextension-polyfill', () => require('sinon-chrome/webextensions'))
+
+const chrome = require('sinon-chrome');
+
 describe("requestSynonym function", () => {
   test("should return a synonym for the provided word", async () => {
     const word = "happy";
     const synonym = await requestSynonym(word);
     expect(synonym).not.toBe("");
   });
+});
+
+
+// Import the code to test
+const {getLanguage} = require('./src/script');
+
+// Test case
+test('language should be retrieved from storage', () => {
+  // Mock the chrome.storage.sync.get method
+  const mockGet = jest.fn().mockImplementation((keys, callback) => {
+    callback({ language: 'en' });
+  });
+  chrome.storage.sync.get = mockGet;
+
+  // Call the function to get the language
+  getLanguage();
+
+  // Expect the language to be retrieved and logged
+  expect(mockGet).toHaveBeenCalledTimes(1);
+  expect(mockGet).toHaveBeenCalledWith(['language'], expect.any(Function));
+  expect(console.log).toHaveBeenCalledWith('Language: en');
 });
