34926f7876771ae79c6a0e2bc678497b91286f3f
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-04-26 12:39:17 +0200

    new test cases

--- script.test.js
+++ script.test.js
@@ -1,5 +1,4 @@
-const { requestSynonym, getLanguage } = require("./src/script");
-const chrome = require('sinon-chrome');
+const { requestSynonym, requestT } = require("./src/script");
 
 describe("requestSynonym function", () => {
   test("should return a synonym for the provided word", async () => {
@@ -9,3 +8,20 @@ describe("requestSynonym function", () => {
   });
 });
 
+describe('requestT', () => {
+  test('should return a synonym for the word passed in', async () => {
+    const word = 'happy';
+    const synonym = await requestT(word);
+
+    expect(typeof synonym).toBe('string'); // check that the synonym is a string
+    expect(synonym).not.toBe('happy'); // check that the synonym is not the same as the original word
+    expect(synonym.length).toBeGreaterThan(0); // check that the synonym is not an empty string
+  });
+
+  test('should return "Please try again" when there is an error', async () => {
+    const word = 'nonexistentword';
+    const synonym = await requestT(word);
+
+    expect(synonym).toBe('Please try again'); // check that the function returns the error message
+  });
+});
