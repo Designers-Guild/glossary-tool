8145fc052025a95d0931e3d1c5f5dcd8fcc6f8ad
Moe75763 <2332271@students.wits.ac.za>
2023-05-14 01:22:56 +0200

    Background colour selection

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const languageSelect = document.getElementById('language');
     const saveButton = document.getElementById('save-button');
-  
+    const backgroundColorDropdown = document.getElementById("background-color");
+
     // Load the saved language preference (if any)
     chrome.storage.sync.get(['language'], function(items) {
       if (items.language) {
@@ -9,12 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(`Selected language: ${languageSelect.value}`);
       }
     });
+    chrome.storage.sync.get(['colour'], function(items) {
+      if (items.colour) {
+        backgroundColorDropdown.value = items.colour;
+      }
+    });
   
     // Save the selected language when the user clicks the save button
     saveButton.addEventListener('click', function() {
       const selectedLanguage = languageSelect.value;
+      const selectedColour = backgroundColorDropdown.value;
       chrome.storage.sync.set({ 'language': selectedLanguage }, function() {
         console.log('Language set to ' + selectedLanguage);
+      });
+      chrome.storage.sync.set({ 'colour': backgroundColorDropdown.value }, function() {
+        console.log('Colour set to ' + backgroundColorDropdown);
       });
       
       window.close(); // Close the popup window
