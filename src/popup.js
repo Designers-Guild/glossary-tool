905c72fa0c5a68f69f9dd05ea1a1aa17770fe354
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-05-14 11:33:15 +0200

    text colors added

--- src/popup.js
+++ src/popup.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const languageSelect = document.getElementById('language');
     const saveButton = document.getElementById('save-button');
     const backgroundColorDropdown = document.getElementById("backgroundColor");
+    const textColorDropdown = document.getElementById("textColor");
+
 
     // Load the saved language preference (if any)
     chrome.storage.sync.get(['language'], function(items) {
@@ -15,18 +17,27 @@ document.addEventListener("DOMContentLoaded", function() {
         backgroundColorDropdown.value = items.backgroundColor;
       }
     });
+
+    chrome.storage.sync.get(['textColor'], function(items) {
+      if (items.textColor) {
+        textColor.value = items.textColor;
+      }
+    });
   
     // Save the selected language when the user clicks the save button
     saveButton.addEventListener('click', function() {
       const selectedLanguage = languageSelect.value;
       const selectedColour = backgroundColorDropdown.value;
+      const selectedTextColor = textColorDropdown.value;
       chrome.storage.sync.set({ 'language': selectedLanguage }, function() {
         console.log('Language set to ' + selectedLanguage);
       });
       chrome.storage.sync.set({ 'backgroundColor': selectedColour}, function() {
         console.log('Colour set to ' +selectedColour);
       });
-      
+      chrome.storage.sync.set({ 'textColor': selectedTextColor}, function() {
+        console.log('Colour set to ' +selectedTextColor);
+      });
       window.close(); // Close the popup window
       
     });
