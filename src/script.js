2f2ff1e4069de47d885ec3b01d496e59a6ba0082
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-05-15 12:07:18 +0200

    latest codecov

--- src/script.js
+++ src/script.js
@@ -25,64 +25,6 @@ else {
 console.log("Language: " + language);
 }
 });
-
-// Read the colour from storage and store it in the global variable
-chrome.storage.sync.get(['backgroundColor'], function(items) {
-  backgroundColor = items.backgroundColor;
-if (typeof backgroundColor === "undefined") {
-// Default colour is "default"
-chrome.storage.sync.set({'backgroundColor': '#6b9fc4'}, function() {
-console.log('Colour set to Default.');
-backgroundColor = '#6b9fc4';
-});
-}
-else {
-root.style.setProperty('--bg-color', backgroundColor.toString());
-console.log('Colour: ' + backgroundColor);
-}
-});
-
- // Read the text color from storage and store it in the global variable
- chrome.storage.sync.get(['textColor'], function(items) {
-  textColor = items.textColor;
-  if (typeof textColor === "undefined") {
-    // Default text color is black
-    chrome.storage.sync.set({'textColor': '#444444'}, function() {
-      console.log('Text color set to Default.');
-      textColor = '#444444';
-    });
-  }
-  else {
-    root.style.setProperty('--text-color', textColor.toString());
-    console.log('Text color: ' + textColor);
-  }
-});
-
-// Listen for changes to the "language" key
-chrome.storage.onChanged.addListener(function(changes, areaName) {
-if (areaName === 'sync' && changes.language) {
-language = changes.language.newValue;
-console.log('Language updated to ' + language);
-}
-});
-
-// Listen for changes to the "colour" key
-chrome.storage.onChanged.addListener(function(changes, areaName) {
-if (areaName === 'sync' && changes.backgroundColor) {
-  backgroundColor = changes.backgroundColor.newValue;
-root.style.setProperty('--bg-color', backgroundColor.toString());
-console.log('Colour updated to ' + backgroundColor);
-}
-});
-
-// Listen for changes to the "colour" key
-chrome.storage.onChanged.addListener(function(changes, areaName) {
-  if (areaName === 'sync' && changes.textColor) {
-    textColor = changes.textColor.newValue;
-  root.style.setProperty('--text-color', textColor.toString());
-  console.log('Colour updated to ' + textColor);
-  }
-  });
 }
 
 // Function connects to OpenAI API and returns a synonym for the word passed in
