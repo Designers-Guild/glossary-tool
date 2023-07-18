3e10ae621342cd7b629e2664c06572d368231001
Salmaan Ebrahim <salmaanebrahim@gmail.com>
2023-07-18 15:14:10 +0200

    test

--- script.test.js
+++ script.test.js
@@ -65,7 +65,7 @@ describe('requestHomonym', () => {
   test('should return a homonym for the word passed in', async () => {
     const word = "happy";
     const context = "I'm glad to see you looking so happy today.";
-    const homonyms = ["haply", "happi", "hapi", "hap","hippie","hippy","Chappy","appy"];
+    const homonyms = ["haply", "happi", "hapi", "hap","hippie","hippy","Chappy","appy","Jubilant","merry"];
 
     // Normalize the synonyms to lowercase without punctuation
     const normalizedHomonyms = homonyms.map(homonym => homonym.toLowerCase().replace(/[.,]/g, ''));
